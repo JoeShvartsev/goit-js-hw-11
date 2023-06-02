b7700ef86cc22d3c47d7ac6de6a560ba0bf3d638
JoeShvartsev <120992324+JoeShvartsev@users.noreply.github.com>
2023-06-02 16:17:14 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css"
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryMarkup } from "./gallerymarkup";
 import { fetchPics } from "./fetch";
 
@@ -11,63 +11,76 @@ export const refs = {
   inputEl: document.querySelector('input[name="searchQuery"]'),
   observer: document.querySelector('.observer'),
   loadBtn: document.querySelector('.load-more')
-}
+};
 
 let currentPage = 1; 
-let currentValue = ''
-isNextPageLoad = false;
+let currentValue = '';
+let isNextPageLoad = false;
+
 const onFormSubmit = (e) => {
   e.preventDefault();
   const inputValue = refs.inputEl.value.trim();
   currentValue = inputValue;
   refs.galleryEl.textContent = '';
-  isNextPageLoad = false
+  isNextPageLoad = false;
   performSearch(currentValue);
   e.currentTarget.reset();
 };
+
 const performSearch = (inputValue) => {
   fetchPics(inputValue, currentPage)
     .then((data) => {
       if (currentPage === Math.ceil(data.totalHits / 3)) {
         observer.unobserve(refs.observer);
         Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-      }else if (data.hits.length === 0) {
+      } else if (data.hits.length === 0) {
         Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        // refs.galleryEl.textContent = ''
-      } else if (!isNextPageLoad){
+      } else if (!isNextPageLoad) {
         galleryMarkup(data);
-        Notiflix.Notify.info(`Hooray! We found  ${data.totalHits} images.`);
+        Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
         galleryLightbox.refresh();
         observer.observe(refs.observer);
-      }
-      else if(isNextPageLoad){
+        scrollToNextGroup();
+      } else if (isNextPageLoad) {
         galleryMarkup(data);
         galleryLightbox.refresh();
         observer.observe(refs.observer);
+        scrollToNextGroup();
       }
     })
     .catch((error) => {
       console.error(error.message);
     });
 };
-let options = {
+
+const options = {
   root: null,
   rootMargin: "300px",
   threshold: 0,
 };
+
 const onLoadMore = (entries, observer) => {
-  entries.forEach((entry)=>{
-    if(entry.isIntersecting){
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
       currentPage++;
-      isNextPageLoad = true; 
+      isNextPageLoad = true;
       performSearch(currentValue);
     }
-  })
+  });
 };
-let observer = new IntersectionObserver(onLoadMore, options);
 
-const optionsEl = {captionData:'alt', captionDelay:'250'};
+const observer = new IntersectionObserver(onLoadMore, options);
+
+const optionsEl = { captionData: 'alt', captionDelay: '250' };
 const galleryLightbox = new SimpleLightbox('.gallery a', optionsEl);
+
+const scrollToNextGroup = () => {
+  const { height: cardHeight } = refs.galleryEl.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+};
 
 refs.formEl.addEventListener('submit', onFormSubmit);
 
